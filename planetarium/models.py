03a806d52f1923fe49a24506d2ffdc04e7177967
Claymore-AI <dimalishchuk2003@gmail.com>
2025-05-15 17:52:12 +0300

    create all models

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=70)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=70)
+    description = models.TextField()
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=70)
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from planetarium.models import ShowSession
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
