05a1a4443fe69dd85067b49e35c4561d5527a1ed
Claymore-AI <dimalishchuk2003@gmail.com>
2025-05-18 22:06:15 +0300

    add swagger

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/planetarium/", include("planetarium.urls", namespace="planetarium")),
     path("api/v1/reservations/", include("reservations.urls", namespace="reservations")),
     path("api/v1/users/", include('users.urls', namespace="users")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+
 ]
