69417b84928da68b367a9b5815856ddbb714efdb
Claymore-AI <dimalishchuk2003@gmail.com>
2025-05-18 20:02:55 +0300

    create endpoints for users

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/planetarium/", include("planetarium.urls", namespace="planetarium")),
-    path("api/v1/reservations/", include("reservations.urls", namespace="reservations"))
+    path("api/v1/reservations/", include("reservations.urls", namespace="reservations")),
+    path("api/v1/users/", include('users.urls', namespace="users")),
 ]
