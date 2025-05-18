3cc88afe975e90b4a1559caacf2a150a2f73f886
Claymore-AI <dimalishchuk2003@gmail.com>
2025-05-18 20:12:49 +0300

    fix problem, add app name for users

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from users.views import UserRegistrationView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
+app_name = "users"
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
